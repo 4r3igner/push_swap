4f6e24724507b4953a1ea8c0de68841826d6777e
Trofim Garaba <trofimgaraba"gmail.com>
2026-03-13 14:10:38 +0100

    bullet proof swpaping opperations

--- push_swap/swap.c
+++ push_swap/swap.c
@@ -1,47 +1,59 @@
 #include "push_swap.h"
 
-void  sa(c_llist  **head_a)
+void  sa(c_llist  **head_a,int flag)
 {
-  if (!head_a || !(*head_a))
+  if (!head_a || !(*head_a) || (*head_a)->next == *head_a)
     return;
 
   c_llist *last;
   c_llist *second;
+  c_llist *first;
+
+  first = *head_a;
   last = (*head_a)->prev;
   second = (*head_a)->next;
 
-  second->prev = last;
+
+  first->next = second->next;
+  second->next->prev = first;
+  first->prev = second;
   last->next = second;
-  (*head_a)->next = second->next;
-  second->next->prev = *head_a;
-  second->next = *head_a;
-  (*head_a)->prev = second;
+  second->prev = last;
+  second->next = first;  
 
   *head_a = second;
+  if(flag != 1)
+    write(1,"sa\n",3);
 }
 
-void  sb(c_llist  **head_b)
+void  sb(c_llist  **head_b, int flag)
 {
-  if (!head_b || !(*head_b))
+  if (!head_b || !(*head_b) || (*head_b)->next == *head_b )
     return;
 
   c_llist *last;
   c_llist *second;
+  c_llist *first;
+
+  first = *head_b;
   last = (*head_b)->prev;
   second = (*head_b)->next;
 
-  second->prev = last;
+  first->next = second->next;
+  second->next->prev = first;
+  first->prev = second;
   last->next = second;
-  (*head_b)->next = second->next;
-  second->next->prev = *head_b;
-  second->next = *head_b;
-  (*head_b)->prev = second;
+  second->prev = last;
+  second->next = first;
 
   *head_b = second;
+  if(flag != 1)
+    write(1,"sb\n",3);
 }
 
 void ss(c_llist  **head_a, c_llist **head_b)
 {
-  sa(&head_a);
-  sb(&head_b);
+  sa(head_a,1);
+  sb(head_b,1);
+  write(1,"ss\n",3);
 }
